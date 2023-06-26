0d438af757e3b7a8a9fd80813cf562b7da1c5528
tertius Jubber <tjubber@gmail.com>
2023-06-26 10:15:54 +0200

    finished problem

---
+++ Problem1.js
@@ -0,0 +1,35 @@
+// A vector type
+
+// Write a class Vec that represents a vector in two-dimensional space. done
+//It takes x and y parameters (numbers), which it should save to properties of the same name.done
+// Give the Vec prototype two methods, plus and minus, done
+// that take another vector as a parameter and - done
+// return a new vector that has the sum or difference of the two vectors’ (this and the parameter) x and y values.
+// Add a getter property length to the prototype that computes the length of the vector—that is, the distance of the point (x, y) from the origin (0, 0).
+
+class Vec {
+  constructor(x,y){
+    this.x = x
+    this.y = y
+  }
+  plus(vector2){
+    let addX = this.x + vector2.x
+    let addY = this.y + vector2.y
+    return new Vec(addX,addY)
+  }
+  minus(vector2){
+    let minusX = this.x - vector2.x
+    let minusY = this.y - vector2.y
+    return new Vec(minusX,minusY)
+  }
+  get length(){
+  return Math.sqrt(this.x**2 +this.y**2)
+  }
+}
+
+//console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+//console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// → 5
